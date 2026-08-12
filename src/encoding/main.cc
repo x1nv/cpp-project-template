7a8fd3c85a0f558f0dfd2d2237909655c0c868b5
x1nv <33998532+x1nv@users.noreply.github.com>
2026-08-12 11:51:53 +0800

    fix: chinese output garbled text

--- src/encoding/main.cc
+++ src/encoding/main.cc
@@ -4,6 +4,8 @@
 #ifdef _DEBUG
 #include <crtdbg.h>
 #endif  // _DEBUG
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 
 #include <print>
 #include <string>
@@ -11,19 +13,31 @@
 #include "absl/strings/str_cat.h"
 #include "src/encoding/say_hello.h"
 
+namespace {
+
+void SetUtf8ConsoleOutput() {
+  SetConsoleOutputCP(CP_UTF8);
+  SetConsoleCP(CP_UTF8);
+}
+
+}  // namespace
+
 int main() {
-  // check heap memory leak
+  // Check heap memory leak.
 #ifdef _DEBUG
   _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-  // set utf-8 character sets
-  // ...
+  // Set utf-8 character sets.
+  // Otherwise, printf will display Chinese characters as garbled text.
+  SetUtf8ConsoleOutput();
 
+  std::println("你好, 世界");
   std::string message = absl::StrCat("hello", " world!");
   std::println("Welcome to this C++ project template!");
   std::println("{}", message);
   hello();
+  system("pause");
 
   return 0;
 }
